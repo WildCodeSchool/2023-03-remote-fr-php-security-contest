cfad3cdd2c1d9ee8abc62a7f79b8c7204b30f595
Guillaume HARARI <guillaume.harari@wildcodeschool.fr>
2020-01-20 08:43:58 +0100

    Add challenges

--- src/DataFixtures/ChallengeFixtures.php
+++ src/DataFixtures/ChallengeFixtures.php
@@ -24,6 +24,13 @@ class ChallengeFixtures extends Fixture
             'notice'   => 'Craque le mot de passe pour capturer le drapeau',
         ],
         [
+            'name'     => '[Cryptanalyse] HTTP - Directory indexing',
+            'url'      => 'http://challenge01.root-me.org/cryptanalyse/ch8/ch8.txt',
+            'solution' => '2ac376481ae546cd689d5b91275d324e',
+            'level'    => 1,
+            'notice'   => 'Decode pour capturer le drapeau',
+        ],
+        [
             'name'     => '[Web client] Javascript - Authentification',
             'url'      => 'http://challenge01.root-me.org/web-client/ch9/',
             'solution' => 'sh.org',
@@ -45,6 +52,13 @@ class ChallengeFixtures extends Fixture
             'notice'   => 'Entre le montant de la balance du compte en banque pour capturer le drapeau',
         ],
         [
+            'name'     => '[Web client] Admin usurpation',
+            'url'      => 'https://www.newbiecontest.org/epreuves/hacking/ep3/index.php?epreuveid=sdqufhdsqf54sq65df4&numid=qsd46ds5qf65d&forum=sdf4ds&admin=false&mysqlid=sdqsq6f46d5sqsdf65ds4q6f46d5sq4f56ds4qf654dsq6f54dqs56f4dsqf4f',
+            'solution' => 'magical',
+            'level'    => 2,
+            'notice'   => 'Fais toi passer pour l\'admin',
+        ],
+        [
             'name'     => '[Web client] Javascript - Source',
             'url'      => 'http://challenge01.root-me.org/web-client/ch1/ch1.html',
             'solution' => '123456azerty',
@@ -52,11 +66,25 @@ class ChallengeFixtures extends Fixture
             'notice'   => 'Craque le mot de passe pour capturer le drapeau',
         ],
         [
+            'name'     => '[XSS] Hack la recherche',
+            'url'      => 'https://xss-game.appspot.com/level1',
+            'solution' => 'Persistence is key',
+            'level'    => 2,
+            'notice'   => 'Injecte une alert(). Une fois réussi, entre le titre exact du niveau 2 de xss-game pour capturer le drapeau',
+        ],
+        [
             'name'     => '[Web server] HTTP - Open redirect',
             'url'      => 'http://challenge01.root-me.org/web-serveur/ch52/',
             'solution' => 'e6f8a530811d5a479812d7b82fc1a5c5',
             'level'    => 3,
             'notice'   => 'Hack la redirection et jette un oeil au network pour capturer le drapeau',
+        ],
+        [
+            'name'     => '[Web client] Usurpation de navigateur',
+            'url'      => 'http://change-browser.hax.w3challs.com/',
+            'solution' => 'W3C{now_that_we_have_the_right_browser_lets_get_the_party_started}',
+            'level'    => 3,
+            'notice'   => 'Fais passer ton navigateur pour un autre',
         ],
         [
             'name'     => '[Web client] Javascript - Obfuscation',
@@ -73,13 +101,12 @@ class ChallengeFixtures extends Fixture
             'notice'   => 'Recherche dans les entrailles du serveur',
         ],
         [
-            'name'     => '[Cryptanalyse] HTTP - Directory indexing',
-            'url'      => 'http://challenge01.root-me.org/cryptanalyse/ch8/ch8.txt',
-            'solution' => 'Encodage ASCII',
-            'level'    => 1,
-            'notice'   => 'Decode pour capturer le drapeau',
+            'name'     => '[XSS] Tchat scripting',
+            'url'      => 'https://xss-game.appspot.com/level2',
+            'solution' => 'That sinking feeling...',
+            'level'    => 4,
+            'notice'   => 'Injecte une alert() dans le tchat. Une fois réussi, entre le titre exact du niveau 3 de xss-game pour capturer le drapeau',
         ],
-
     ];
 
     public function load(ObjectManager $manager)
